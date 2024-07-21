122f86aefe9b11e9ac9df379127786123c379aab
SACHIN PRAJAPATI <skprajapati3214@gmail.com>
2024-07-21 22:57:39 +0530

    resolving cors issue

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,25 @@
-const connectToMongo=require('./db.js');
+const connectToMongo = require('./db.js');
 connectToMongo();
 const express = require('express');
-var cors=require('cors');
+const cors = require('cors');
 
 const app = express();
 const port = 5000;
+
+// Use CORS middleware
 app.use(cors({
-    origin: '*', // Specify the origin you want to allow
+    origin: '*', // Allow all origins
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Enable this if you need to send cookies or authorization headers
     optionsSuccessStatus: 204
-  }));
-app.use(express.json())
-app.use('/api/auth',require('./routes/auth'))
-app.use('/api/notes',require('./routes/notes'))
+}));
+
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
 app.listen(port, () => {
- console.log(`inotebbok backend app listening at http://localhost:${port}`);
+    console.log(`inotebbok backend app listening at http://localhost:${port}`);
 });
